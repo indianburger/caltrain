aa86b2e92753a23dfa2b24134c12f5fb79b69826
Indian Burger <garavindr85+github@gmail.com>
2011-10-02 22:19:24 -0700

    added basic phonegap hello world

---
+++ android/src/org/caltrain/MainActivity.java
@@ -0,0 +1,14 @@
+package org.caltrain;
+
+import android.os.Bundle;
+
+import com.phonegap.DroidGap;
+
+public class MainActivity extends DroidGap {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+}
